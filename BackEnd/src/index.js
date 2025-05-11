5c1d769d7d945505eb903a7b0b2186236430ebf8
Khandaker-Mohyet <mohyet008@gmail.com>
2025-05-11 22:51:44 +0600

    change user Name

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -7,15 +7,14 @@ import { connectDB } from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
+import { app, server } from "./lib/socket.js";
 
 dotenv.config()
-const app = express()
+// const app = express()
 
 const PORT = process.env.PORT
 
-// app.use(express.json())
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
   origin: "http://localhost:5173",
@@ -25,7 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB()
 });
